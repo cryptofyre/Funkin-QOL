b2b7c15855dc4bb89a694be9179168f29402daad
Fire <iifir3z@protonmail.com>
2021-02-06 19:20:07 -0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TitleState, GameFramerate = 240));
+		addChild(new FlxGame(0, 0, TitleState, GameFramerate = 240, Zoom = 1));
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));

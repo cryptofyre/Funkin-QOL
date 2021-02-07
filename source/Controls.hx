3ae34f70dca5c279ceefccc9ea2d85021bf3c36c
Fire <iifir3z@protonmail.com>
2021-02-06 18:13:48 -0800

    reverting the keybind change temporarily

--- source/Controls.hx
+++ source/Controls.hx
@@ -10,7 +10,6 @@ import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
-import flixel.system.macros.FlxMacroUtil;
 
 #if (haxe >= "4.0.0")
 enum abstract Action(String) to String from String
@@ -206,8 +205,6 @@ class Controls extends FlxActionSet
 
 	inline function get_CHEAT()
 		return _cheat.check();
-
-	public static var keyboardMap:Map<String, FlxKey> = new Map();
 
 	#if (haxe >= "4.0.0")
 	public function new(name, scheme = None)
@@ -495,21 +492,20 @@ class Controls extends FlxActionSet
 		if (reset)
 			removeKeyboard();
 
-		FlxG.save.bind('funkin', 'ninjamuffin99');
-		loadControls();
 		keyboardScheme = scheme;
 
 		#if (haxe >= "4.0.0")
 		switch (scheme)
 		{
 			case Solo:
-				for(cont in Type.allEnums(Controls.Control))
-				{
-					if(cont != Control.CHEAT){
-						inline bindKeys(cont,[keyboardMap[Std.string(cont)]]);
-						inline bindKeys(cont,[keyboardMap[Std.string(cont) + ' (ALTERNATE)']]);
-					}
-				}
+				inline bindKeys(Control.UP, [W, FlxKey.UP]);
+				inline bindKeys(Control.DOWN, [S, FlxKey.DOWN]);
+				inline bindKeys(Control.LEFT, [A, FlxKey.LEFT]);
+				inline bindKeys(Control.RIGHT, [D, FlxKey.RIGHT]);
+				inline bindKeys(Control.ACCEPT, [Z, SPACE, ENTER]);
+				inline bindKeys(Control.BACK, [BACKSPACE, ESCAPE]);
+				inline bindKeys(Control.PAUSE, [P, ENTER, ESCAPE]);
+				inline bindKeys(Control.RESET, [R]);
 			case Duo(true):
 				inline bindKeys(Control.UP, [W]);
 				inline bindKeys(Control.DOWN, [S]);
@@ -565,30 +561,6 @@ class Controls extends FlxActionSet
 			case Custom: // nothing
 		}
 		#end
-	}
-
-	static function loadControls(){
-		if (FlxG.save.data.keyboardMap != null)
-		{
-			keyboardMap = FlxG.save.data.keyboardMap;
-		}
-		else
-		{
-			var keyMaps:Map<String, FlxKey> = FlxMacroUtil.buildMap("flixel.input.keyboard.FlxKey");
-
-			var controlsStrings = CoolUtil.coolTextFile('assets/data/controls.txt');
-
-			keyboardMap = new Map();
-			for (i in 0...controlsStrings.length){
-				var elements:Array<String> = controlsStrings[i].split(',');
-				keyboardMap.set(elements[0],keyMaps[elements[1]]);
-			}
-		}
-	}
-
-	public static function saveControls(){
-		FlxG.save.data.keyboardMap = keyboardMap;
-		FlxG.save.flush();
 	}
 
 	function removeKeyboard()
--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -8,10 +8,8 @@ import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxMath;
-import flixel.system.macros.FlxMacroUtil;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flixel.effects.FlxFlicker;
 import lime.utils.Assets;
 
 class OptionsMenu extends MusicBeatState
@@ -22,7 +20,6 @@ class OptionsMenu extends MusicBeatState
 	var controlsStrings:Array<String> = [];
 
 	private var grpControls:FlxTypedGroup<Alphabet>;
-	var changingInput:Bool = false;
 
 	override function create()
 	{
@@ -37,28 +34,18 @@ class OptionsMenu extends MusicBeatState
 
 		grpControls = new FlxTypedGroup<Alphabet>();
 		add(grpControls);
-		var i = 0;
 
-		for(key => value in Controls.keyboardMap){
-			var elements:Array<String> = controlsStrings[i].split(',');
-			var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30,'set ' + key + ': ' + value, true, false);
-			controlLabel.isMenuItem = true;
-			controlLabel.targetY = i;
-			grpControls.add(controlLabel);
-			i++;
+		for (i in 0...controlsStrings.length)
+		{
+			if (controlsStrings[i].indexOf('set') != -1)
+			{
+				var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, controlsStrings[i].substring(3) + ': ' + controlsStrings[i + 1], true, false);
+				controlLabel.isMenuItem = true;
+				controlLabel.targetY = i;
+				grpControls.add(controlLabel);
+			}
+			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
 		}
-
-		//for (i in 0...controlsStrings.length)
-		//{
-		//
-		//	var elements:Array<String> = controlsStrings[i].split(',');
-		//	var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30,'set ' + elements[0] + ': ' + elements[1], true, false);
-		//	controlLabel.isMenuItem = true;
-		//	controlLabel.targetY = i;
-		//	grpControls.add(controlLabel);
-		//
-		//	// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
-		//}
 
 		super.create();
 	}
@@ -67,23 +54,40 @@ class OptionsMenu extends MusicBeatState
 	{
 		super.update(elapsed);
 
-		if(!changingInput)
+		if (controls.ACCEPT)
 		{
-			if (controls.BACK){
-				FlxG.switchState(new MainMenuState());
-				Controls.saveControls();
-				controls.setKeyboardScheme(Solo,true);
-			}
-			if(controls.UP_P)
-				changeSelection(-1);
-			if(controls.DOWN_P)
-				changeSelection(1);
-			if(controls.ACCEPT)
-				ChangeInput();
+			changeBinding();
 		}
+
+		if (isSettingControl)
+			waitingInput();
 		else
 		{
-			ChangingInput();
+			if (controls.BACK)
+				FlxG.switchState(new MainMenuState());
+			if (controls.UP_P)
+				changeSelection(-1);
+			if (controls.DOWN_P)
+				changeSelection(1);
+		}
+	}
+
+	function waitingInput():Void
+	{
+		if (FlxG.keys.getIsDown().length > 0)
+		{
+			PlayerSettings.player1.controls.replaceBinding(Control.LEFT, Keys, FlxG.keys.getIsDown()[0].ID, null);
+		}
+		// PlayerSettings.player1.controls.replaceBinding(Control)
+	}
+
+	var isSettingControl:Bool = false;
+
+	function changeBinding():Void
+	{
+		if (!isSettingControl)
+		{
+			isSettingControl = true;
 		}
 	}
 
@@ -108,65 +112,17 @@ class OptionsMenu extends MusicBeatState
 
 		for (item in grpControls.members)
 		{
-			#if !switch
-			NGio.logEvent('Fresh');
-			#end
+			item.targetY = bullShit - curSelected;
+			bullShit++;
 
-			FlxG.sound.play('assets/sounds/scrollMenu' + TitleState.soundExt, 0.4);
+			item.alpha = 0.6;
+			// item.setGraphicSize(Std.int(item.width * 0.8));
 
-			curSelected += change;
-
-			if (curSelected < 0)
-				curSelected = grpControls.length - 1;
-			if (curSelected >= grpControls.length)
-				curSelected = 0;
-
-			// selector.y = (70 * curSelected) + 30;
-
-			var bullShit:Int = 0;
-
-			for (item in grpControls.members)
+			if (item.targetY == 0)
 			{
 				item.alpha = 1;
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
-
-	function ChangeInput()
-	{
-		changingInput = true;
-		FlxFlicker.flicker(grpControls.members[curSelected],0);
 	}
-
-	function ChangingInput()
-	{
-		if(FlxG.keys.pressed.ANY){
-			//Checks all known keys
-			var keyMaps:Map<String, FlxKey> = FlxMacroUtil.buildMap("flixel.input.keyboard.FlxKey");
-			for(key in keyMaps.keys())
-			{
-				if(FlxG.keys.checkStatus(key,2) && key != "ANY")
-				{
-					FlxFlicker.stopFlickering(grpControls.members[curSelected]);
-
-					var elements:Array<String> = grpControls.members[curSelected].text.split(':');
-					var name:String = StringTools.replace(elements[0],'set ','');
-					var controlLabel:Alphabet = new Alphabet(0, 0,'set ' + name + ': ' + key, true, false);
-					controlLabel.isMenuItem = true;
-					controlLabel.targetY = 0;
-
-					grpControls.replace(grpControls.members[curSelected],controlLabel);
-					changingInput = false;
-
-					Controls.keyboardMap.set(name,keyMaps[key]);
-					FlxG.log.add(name + " is bound to " + keyMaps[key]);
-
-
-					break;
-				}
-			}
-
-		}
-	}
-}
 }
